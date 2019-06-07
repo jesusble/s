6d35669930e372bfcd3aa7cf84d3b73227bed2c3
jesusble <keziahblessy33@gmail.com>
2019-06-07 15:37:23 +0530

    Create insert.py

---
+++ insert.py
@@ -0,0 +1,11 @@
+s,t=map(str,input().split())
+l1=0
+if len(s)>len(t):
+  s,t=t,s
+i=0
+while i<len(s):
+  l1+=(ord(t[i])-ord(s[i]))
+  i+=1
+for i in range(i,len(t)):
+  l1+=ord(t[i])-ord('a')+1
+print(l1)
